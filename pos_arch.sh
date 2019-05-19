d7405a35aca32dea65af59fcc8f905c17e612a64
RONALDO ARAUJO <48135044+roh031@users.noreply.github.com>
2019-05-19 09:55:33 -0300

    Update pos_arch.sh

--- pos_arch.sh
+++ pos_arch.sh
@@ -1,39 +1,59 @@
 #!/usr/bin/env bash
 echo "Pós instalação Arch Linux + XFCE"
-sleep 3
+sleep 2
 #
 # Configurar o teclado
 echo "KEYMAP=br-abnt2" >> /etc/vconsole.conf
 echo "FONT=lat0-16" >> /etc/vconsole.conf
 #
 # Adicionar seu usuário
-useradd -m -g users -G wheel,storage,video,audio,power,optical,network -s /bin/bash ronaldo
+echo "Criando usuário tux"
+sleep 2
+useradd -m -g users -G wheel,storage,video,audio,power,optical,network -s /bin/bash tux
 #
 # Colocar a senha
-passwd ronaldo
+echo "Adicionado senha ao usuário tux"
+sleep 2
+passwd tux
 #
 # Instalar o sudo
+echo "Instalando sudo"
+sleep 2
 pacman -S sudo --noconfirm
 #
 # Instalar o servidor gráfico
+echo "Instalando servidor gráfico X.org"
+sleep 2
 pacman -S xorg xorg-xinit xf86-video-vesa xf86-video-fbdev virtualbox-guest-utils ttf-dejavu gvfs-mtp  --noconfirm
 #
 # Instalar o suporte ao audio
+echo "Instalando drive de áudio"
+sleep 2
 pacman -S alsa-lib alsa-utils alsa-firmware alsa-plugins pulseaudio-alsa pulseaudio paprefs  --noconfirm
 #
 # Instalar compactadores/descompactadores
-pacman -S tar file-roller gzip bzip2 unzip unrar p7zip --noconfirm
+echo "Instalando compactadores"
+sleep 2
+pacman -S tar file-roller unzip unrar p7zip --noconfirm
 #
 # Suporte a ntfs e servidor SSH
-pacman -S ntfs-3g  --noconfirm 
+echo "Instalando servidor OpenSSH e suporte ao NTFS"
+sleep 2
+pacman -S ntfs-3g openssh sshfs   --noconfirm 
 #
 # Suporte ao GIT 
-pacman -S git wget curl openssh sshfs --noconfirm
+echo "Instalando GIT, CURL e wget"
+sleep 2
+pacman -S git wget curl  --noconfirm
 #
-# Instalar o KDE Plasma 5.15
+# Instalar o XFCE4
+echo "Instalando a interface gráfica XFCE"
+sleep 2
 pacman -S xfce4 xfce4-goodies  --noconfirm 
 #
 # Instalar Lightdm 
+echo "Instalando gerenciador de login"
+sleep 2
 pacman -S lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings   --noconfirm 
 
 # Criar o arquivo XINITRC
@@ -43,9 +63,11 @@ chgrp ronaldo /home/ronaldo/.xinitrc
 chmod 644 /home/ronaldo/.xinitrc
 
 # Instalar utilitários
+echo "Instalando vários utilitários"
+sleep 2
 pacman -S  dcfldd wipe testdisk foremost smbclient samba neofetch clamav  scrot ddrescue dvd+rw-tools htop gparted uget --noconfirm
 pacman -S transmission-qt gftp firefox firefox-i18n-pt-br  chromium  evince devede brasero libreoffice gthumb --noconfirm
-pacman -S audacious vlc papirus-icon-theme audacity  systemd-swap emelfm2 moc feh simplescreenrecorder --noconfirm 
+pacman -S audacious vlc papirus-icon-theme moka-icon-theme audacity  systemd-swap emelfm2 moc feh simplescreenrecorder --noconfirm 
 pacman -S ffmpeg imagemagick gnome-disk-utility gparted  galculator --noconfirm 
 #
 # Habilitar a tela de login
